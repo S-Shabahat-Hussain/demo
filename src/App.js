7dd4cfd3e7ec6d68aa6abd1966ebaf00d4330210
S-Shabahat-Hussain <sshabahathussain@gmail.com>
2024-04-16 11:25:26 +0530

    initial commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[100vw] h-[100vh] flex bg-[#378ef9] text-white'>
+      <div>Increment && Decrement</div>
+      <div>
+        <button> - </button>
+        <div>
+
+        </div>
+        <button> + </button>
+      </div>
+        <button>Reset</button>
     </div>
   );
 }
